35243f767930157e428d824e912552e8bf754730
Alina Voilova <91426818+Pizza2Pizza@users.noreply.github.com>
2023-07-24 18:56:08 +0200

    created code for ER database

--- er_generator.py
+++ er_generator.py
@@ -3,35 +3,40 @@ import networkx as nx
 
 #importing the matplotlib library for plotting the graph
 import matplotlib.pyplot as plt
+import json
 
+for i in range(10):
+    n = 11  # 10 nodes
+    seed = 20161  # seed random number generators for reproducibility
 
-n = 11  # 10 nodes
-seed = 20161  # seed random number generators for reproducibility
+    while(1):
+        # Use seed for reproducibility
+        G = nx.erdos_renyi_graph(n, 0.2)
+        if not(nx.is_connected(G)):
+            pass
+        else:
+            break
 
-while(1):
-    # Use seed for reproducibility
-    G = nx.erdos_renyi_graph(n, 0.2)
-    if not(nx.is_connected(G)):
-        pass
-    else:
-        break
+    # some properties
+    print("node degree clustering")
+    for v in nx.nodes(G):
+        print(f"{v} {nx.degree(G, v)} {nx.clustering(G, v)}")
 
-# some properties
-print("node degree clustering")
-for v in nx.nodes(G):
-    print(f"{v} {nx.degree(G, v)} {nx.clustering(G, v)}")
+    print()
+    print("the adjacency list")
+    for line in nx.generate_adjlist(G):
+        print(line)
 
-print()
-print("the adjacency list")
-for line in nx.generate_adjlist(G):
-    print(line)
+    pos = nx.spring_layout(G, seed=seed)  # Seed for reproducible layout
+    nx.draw(G, pos=pos,with_labels=True)
+    #plt.show()
 
-pos = nx.spring_layout(G, seed=seed)  # Seed for reproducible layout
-nx.draw(G, pos=pos,with_labels=True)
-plt.show()
+    d = nx.to_dict_of_dicts(G)
 
+    # file=open("Graph_created.txt","wb")
+    # nx.write_adjlist(d,file,encoding='utf-8')
+    # file.close()
 
-
-file=open("Graph_created.txt","wb")
-nx.write_adjlist(G,file,encoding='utf-8')
-file.close()
+    with open("Graph_created.txt",'a') as data: 
+        data.write(json.dumps(d))
+        data.write("\n")
--- forward.py
+++ forward.py
@@ -7,36 +7,59 @@ import matplotlib.pyplot as plt
 from numpy import random
 from random import sample
 
-days = 3
+import csv
+import json
 
-file = "Graph_created.txt"
-G = nx.read_adjlist(file)
+#open the file to read from
+file_from = open("Graph_created.txt",'r')
 
-pos = nx.spring_layout(G)  # Seed for reproducible layout
-nx.draw(G, pos=pos,with_labels=True)
-plt.show()
+data_to_write = []
 
-infected = []
-patient_zero = sample(list(G.nodes()),1)
-infected = [patient_zero[0]] #othwerwise we get a reference
-print("Patient zero is :", infected)
+for line in file_from:
+    days = random.randint(2,10)
+    
+    dictionary = json.loads(line)
+    G = nx.from_dict_of_dicts(dictionary)
 
-immune = [patient_zero[0]]
-new_infected = []
+    pos = nx.spring_layout(G)  # Seed for reproducible layout
+    nx.draw(G, pos=pos,with_labels=True)
+    #plt.show()
 
-for d in range(days):
-    for i in infected:
-        for n in G.neighbors(i):
-            if n not in immune:
-                roll = random.binomial(n=1, p=0.5, size=1)
-                if roll == 1:
-                    new_infected.append(n)
-                    immune.append(n)
-                    print("the current node is", n )
-        #after checking all neigbours we need delete the "parent node" from infected
-        #and move it to the array "seen"
+    infected = []
+    patient_zero = sample(list(G.nodes()),1)
+    infected = [patient_zero[0]] #othwerwise we get a reference
+    print("Patient zero is :", infected)
 
-    infected = new_infected
+    immune = [patient_zero[0]]
     new_infected = []
-print(immune)
-print(infected)
+
+    for d in range(days):
+        for i in infected:
+            for n in G.neighbors(i):
+                if n not in immune:
+                    roll = random.binomial(n=1, p=0.7, size=1)
+                    if roll == 1:
+                        new_infected.append(n)
+                        immune.append(n)
+                        print("the current node is", n )
+            #after checking all neigbours we need delete the "parent node" from infected
+            #and move it to the array "seen"
+
+        infected = new_infected
+        new_infected = []
+    print(immune)
+    print(infected)
+    
+    data_to_write.append([dictionary,infected,immune,days,patient_zero])
+
+#writing forwared data into csv file with format of:
+#adjacency_list,infected_list,immune_list,n_steps,patient zero
+with open('forwarded_graph.csv', 'a', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    # write multiple rows
+    writer.writerows(data_to_write)
+
+#closing file
+file_from.close()
+    
