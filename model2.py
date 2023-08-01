befc0aea68f65c5c444708950d83710637f4b016
Alina Voilova <91426818+Pizza2Pizza@users.noreply.github.com>
2023-08-01 20:04:41 +0200

    test accuracy implemented.

--- model2.py
+++ model2.py
@@ -2,6 +2,7 @@ import torch
 from torch.nn import Linear
 from torch_geometric.nn import GCNConv
 from dataset import MyOwnDataset
+import numpy as np 
 # Helper function for visualization.
 
 import networkx as nx
@@ -43,6 +44,7 @@ def visualize_embedding(h, color, epoch=None, loss=None):
 # Loading the dataset
 print("Loading dataset...")
 train_dataset = MyOwnDataset(root="data/")
+test_dataset = MyOwnDataset(root="data/", test = True)
 model = GCN(train_dataset)
 print(model)
 
@@ -51,7 +53,7 @@ from IPython.display import Javascript,display  # Restrict height of output cell
 display(Javascript('''google.colab.output.setIframeHeight(0, true, {maxHeight: 430})'''))
 
 
-criterion = torch.nn.BCELoss()  # Define loss criterion.
+criterion = torch.nn.BCEWithLogitsLoss()  # Define loss criterion.
 optimizer = torch.optim.Adam(model.parameters(), lr=0.01)  # Define optimizer.
 
 #data = train_dataset[0]
@@ -63,9 +65,40 @@ def train(data):
     optimizer.step()  # Update parameters based on gradients.
     return loss, h
 
+
+def test():
+      model.eval()
+      
+      total_correct = 0
+      total_nodes = 0
+      error = []
+      total_num_nodes  = 0
+      # Check against ground-truth labels.
+      for idx in range (0,10):
+        data = test_dataset[idx]
+        out,h = model(data.x, data.edge_index)
+        # Use the class with highest probability.
+        pred = torch.round(out)
+        test_correct = np.count_nonzero(pred == data.y)
+        # for was_correct in test_correct:
+        #     total_nodes += 1
+        #     if was_correct:
+        #         total_correct += 1
+        mismatches = pred.size(dim = 0) - test_correct
+        total_num_nodes = total_num_nodes+pred.size(dim = 0)
+        error.append(mismatches)
+        
+      # Derive ratio of correct predictions.
+      test_not_acc = sum(error) / total_num_nodes
+      test_correct = 1- test_not_acc
+      return test_correct
+
 for epoch in range(10):
     data = train_dataset[epoch]
     loss, h = train(data)
     if epoch % 10 == 0:
         visualize_embedding(h, color=data.y, epoch=epoch, loss=loss)
         time.sleep(0.3)
+
+test_acc = test()
+print(f'Test Accuracy: {test_acc:.4f}')
